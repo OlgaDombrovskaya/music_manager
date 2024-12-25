f74e04d80764e74057978a1971237b4e8bec6c8e
Olga Dombrovskaya <olgaekb26@gmail.com>
2024-12-25 18:17:17 +0000

    #1: create demo PlaylistManager, test objects were tested

--- src/main/java/com/musicapp/Main.java
+++ src/main/java/com/musicapp/Main.java
@@ -2,6 +2,14 @@ package com.musicapp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PlaylistManager manager = new PlaylistManager();
+
+        Song song1 = new Song("Bohemian Rhapsody", "Queen", 354);
+        Song song2 = new Song("Imagine", "John Lennon", 187);
+
+        manager.addSong(song1);
+        manager.addSong(song2);
+
+        manager.printPlaylist();
     }
 }
